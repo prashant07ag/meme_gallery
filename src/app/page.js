2406045814d2e27806fb88b8701231b293841dfe
Prashant <pa99119@gmail.com>
2024-01-31 18:15:55 +0530

    fixed some bugs

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import { Gallery, Item } from 'react-photoswipe-gallery';
 
 const Home = () => {
   const [memes, setMemes] = useState([]);
+  const [index, setIndex] = useState(0);
   const [after, setAfter] = useState('');
 
   useEffect(() => {
@@ -36,40 +37,44 @@ const Home = () => {
   };
 
   return (
-    <Gallery>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 p-2 ml-2" onScroll={handleScroll}>
-        {memes.map((meme, i) => (
-          <Item
-            key={meme.data.id}
-            original={meme.data.url_overridden_by_dest}
-            thumbnail={meme.data.url_overridden_by_dest}
-            width="768"
-            height="768"
-          >
-            {({ ref, open }) => (
-              <div
-                key={meme.data.id}
-                className="p-4 rounded-md hover:shadow-md transition-transform hover:scale-105 cursor-pointer"
-                style={{ position: 'relative', overflow: 'hidden' }} // Add custom styles here
-                onClick={() => {
-                  setIndex(i);
-                  open();
-                }}
-              >
-                <img
-                  src={meme.data.url_overridden_by_dest}
-                  alt={`Meme ${i}`}
-                  ref={ref}
-                  className="w-full h-auto rounded-md mb-2"
-                />
-                <p className="text-sm font-medium">{meme.data.title}</p>
-                <p className="text-xs text-gray-500">By {meme.data.author}</p>
-              </div>
-            )}
-          </Item>
-        ))}
-      </div>
-    </Gallery>
+    <div>
+      <h1 className="text-4xl font-bold text-center my-5">Reddit Memes Gallery</h1>
+      <Gallery>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 p-2 ml-2" onScroll={handleScroll}>
+          {memes.map((meme, i) => (
+            <Item
+              key={meme.data.id}
+              original={meme.data.url_overridden_by_dest}
+              thumbnail={meme.data.url_overridden_by_dest}
+              width="768"
+              height="768"
+            >
+              {({ ref, open }) => (
+                <div
+                  key={meme.data.id}
+                  className="p-4 rounded-md hover:shadow-md transition-transform hover:scale-105 cursor-pointer"
+                  style={{ position: 'relative', overflow: 'hidden' }} // Add custom styles here
+                  onClick={() => {
+                    setIndex(i);
+                    open();
+                  }}
+                >
+                  <img
+                    src={meme.data.url_overridden_by_dest}
+                    alt={`Meme ${i}`}
+                    ref={ref}
+                    className="w-full h-auto rounded-md mb-2"
+                  />
+                  <p className="text-sm font-medium">{meme.data.title}</p>
+                  <p className="text-xs text-gray-500">By {meme.data.author}</p>
+                </div>
+              )}
+            </Item>
+          ))}
+        </div>
+      </Gallery>
+    </div>
+
   );
 };
 
